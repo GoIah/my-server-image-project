a9daa02e38fbb490c7a4b954c48ecb4f8a95c039
GoIah <viataliifedyn@gmail.com>
2025-08-02 18:01:48 -0500

    added asseets

--- bridge/server.js
+++ bridge/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const sharp = require("sharp");
+const fs = require('fs')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -6,23 +8,35 @@ app.use(express.json());
 
 let latestData = null;
 
-// Endpoint for JavaScript to send data
-app.post("/js-to-server", (req, res) => {
-  latestData = req.body;
-  console.log("Received from JS:", latestData);
-  res.sendStatus(200);
+app.get('/pixels', async (req, res) => {
+    const image = await sharp('image.jpg')
+        .resize(100, 100)
+        .raw()
+        .toBuffer({ resolveWithObject: true });
+
+    const { data, info } = image;
+    const { width, height } = info;
+
+    const pixels = [];
+    for (let y = 0; y < height; y++) {
+        const row = [];
+        for (let x = 0; x < width; x++) {
+            const i = (y * width + x) * 3;
+            const r = data[i];
+            const g = data[i + 1];
+            const b = data[i + 2];
+            row.push([r, g, b]);
+        }
+        pixels.push(row);
+    }
+
+    res.json({ width, height, pixels });
 });
 
-// Endpoint for Roblox to get data
-app.get("/roblox-get-data", (req, res) => {
-  res.json(latestData || {});
-});
-
-// Basic homepage
 app.get("/", (req, res) => {
-  res.send("Roblox Data Bridge is online");
+  res.send("status: online");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running: http://localhost:${PORT}`);
 });
